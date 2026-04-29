bc0a8114f1fb4b21a0ed4a6c8dc207e4576f9d56
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-04-28 22:43:05 -0300

    refactor: add helmet to prod, documentation swagger and provider

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,65 @@
 import fastifyCors from "@fastify/cors";
+import helmet from "@fastify/helmet";
+import fastifySwagger from "@fastify/swagger";
+import scalarApiReference from "@scalar/fastify-api-reference";
 import "dotenv/config";
 import fastify from "fastify";
+import {
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { envToLogger } from "./logger/env-to-logger";
+import type { LoggerEnvType } from "./logger/logger-env-dto";
 import { jwtPlugin } from "./plugins/jwt.plugins";
 import { routes } from "./routes";
 
+const app = fastify({
+	logger: envToLogger[process.env.NODE_ENV as keyof LoggerEnvType] ?? true,
+	requestIdHeader: false,
+	requestIdLogLabel: "request-id",
+}).withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+if (process.env.NODE_ENV === "production") app.register(helmet);
+
+app.register(fastifyCors, {
+	origin: true,
+	methods: ["GET", "POST", "OPTIONS"],
+	allowedHeaders: ["Content-Type", "Authorization"],
+	credentials: true,
+	optionsSuccessStatus: 204,
+});
+
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: "Auth API",
+			description: "API de autenticação e refresh token",
+			version: "1.0.0",
+		},
+	},
+	transform: jsonSchemaTransform,
+});
+
+app.register(scalarApiReference, {
+	routePrefix: "/docs",
+});
+
+app.register(jwtPlugin);
+app.setErrorHandler((error, request, reply) => {
+	request.log.error(error);
+	reply.status(500).send({ error: "Internal server error" });
+});
+
+app.register(routes, {
+	prefix: "/api/v1",
+});
+
 async function bootstrap() {
-	const app = fastify({ logger: true });
-	process.env.NODE_ENV === "development"
-		? "development"
-		: process.env.NODE_ENV === "production"
-			? "production"
-			: "test";
-
-	app.register(fastifyCors, {
-		origin: true,
-		methods: ["GET", "POST", "OPTIONS"],
-		allowedHeaders: ["Content-Type", "Authorization"],
-		credentials: true,
-		optionsSuccessStatus: 204,
-	});
-
-	app.register(jwtPlugin);
-
-	app.register(routes);
-
 	app.listen(
 		{
 			port: Number(process.env.PORT) || 3333,
@@ -34,13 +70,10 @@ async function bootstrap() {
 				console.error(err);
 				process.exit(1);
 			}
-			console.log(`Server is running on ${address}`);
 			app.log.info(`Server is running on ${address}`);
+			app.log.info(`Docs is running on ${address}/docs`);
 		}
 	);
 }
 
-bootstrap().catch(error => {
-	console.error(error);
-	process.exit(1);
-});
+bootstrap();
