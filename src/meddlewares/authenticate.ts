16cdf2dfc836673a358a82285f0e04cd346a5f07
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-05-08 23:09:43 -0300

    chore: add again change status code 401

--- src/meddlewares/authenticate.ts
+++ src/meddlewares/authenticate.ts
@@ -1,12 +1,12 @@
-import type { FastifyRequest, FastifyReply } from "fastify";
+import type { FastifyReply, FastifyRequest } from "fastify";
 
 export async function authenticate(
-	request: FastifyRequest,
-	reply: FastifyReply
+  request: FastifyRequest,
+  reply: FastifyReply,
 ): Promise<void> {
-	try {
-		await request.jwtVerify();
-	} catch (err: unknown) {
-		reply.status(401).send({ message: (err as Error).message });
-	}
+  try {
+    await request.jwtVerify();
+  } catch (err: unknown) {
+    return reply.code(401).send({ error: (err as Error).message });
+  }
 }
