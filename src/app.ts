c63e9ebd90417506fe04b3399a0fada282346c87
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-05-08 23:13:07 -0300

    refactor: add path absolute to module routes

--- src/app.ts
+++ src/app.ts
@@ -5,20 +5,20 @@ import scalarApiReference from "@scalar/fastify-api-reference";
 import "dotenv/config";
 import fastify from "fastify";
 import {
-	jsonSchemaTransform,
-	serializerCompiler,
-	validatorCompiler,
-	type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { envToLogger } from "./logger/env-to-logger";
 import type { LoggerEnvType } from "./logger/logger-env-dto";
 import { jwtPlugin } from "./plugins/jwt.plugins";
-import { routes } from "./routes";
+import { routes } from "./routes/index";
 
 const app = fastify({
-	logger: envToLogger[process.env.NODE_ENV as keyof LoggerEnvType] ?? true,
-	requestIdHeader: false,
-	requestIdLogLabel: "request-id",
+  logger: envToLogger[process.env.NODE_ENV as keyof LoggerEnvType] ?? true,
+  requestIdHeader: false,
+  requestIdLogLabel: "request-id",
 }).withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
@@ -27,69 +27,69 @@ app.setSerializerCompiler(serializerCompiler);
 if (process.env.NODE_ENV === "production") app.register(helmet);
 
 app.register(fastifyCors, {
-	origin: true,
-	methods: ["GET", "POST", "OPTIONS"],
-	allowedHeaders: ["Content-Type", "Authorization"],
-	credentials: true,
-	optionsSuccessStatus: 204,
+  origin: true,
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 204,
 });
 
 app.register(fastifySwagger, {
-	openapi: {
-		info: {
-			title: "Auth API",
-			description: "API de autenticação e refresh token",
-			version: "1.0.0",
-		},
-		servers: [
-			{
-				url: `http://localhost:${process.env.PORT}`,
-				description: "Development Server",
-			},
-		],
-		components: {
-			securitySchemes: {
-				bearerAuth: {
-					type: "http",
-					scheme: "bearer",
-					bearerFormat: "JWT",
-					description: "Authorization",
-				},
-			},
-		},
-	},
-	transform: jsonSchemaTransform,
+  openapi: {
+    info: {
+      title: "Auth API",
+      description: "API de autenticação e refresh token",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT}`,
+        description: "Development Server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+          description: "Authorization",
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
 });
 
 app.register(scalarApiReference, {
-	routePrefix: "/docs",
+  routePrefix: "/docs",
 });
 
 app.register(jwtPlugin);
 app.setErrorHandler((error, request, reply) => {
-	request.log.error(error);
-	reply.code(500).send({ error: "Internal server error" });
+  request.log.error(error);
+  reply.code(500).send({ error: "Internal server error" });
 });
 
 app.register(routes, {
-	prefix: "/api/v1",
+  prefix: "/api/v1",
 });
 
 async function bootstrap() {
-	app.listen(
-		{
-			port: Number(process.env.PORT) || 3333,
-			host: "0.0.0.0",
-		},
-		(err, address) => {
-			if (err) {
-				console.error(err);
-				process.exit(1);
-			}
-			app.log.info(`Server is running on ${address}`);
-			app.log.info(`Docs is running on ${address}/docs`);
-		}
-	);
+  app.listen(
+    {
+      port: Number(process.env.PORT) || 3333,
+      host: "0.0.0.0",
+    },
+    (err, address) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      app.log.info(`Server is running on ${address}`);
+      app.log.info(`Docs is running on ${address}/docs`);
+    },
+  );
 }
 
 bootstrap();
