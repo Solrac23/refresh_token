24000d568beab0b1da2e6f20c450718e42948834
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-05-29 14:11:09 -0300

    refactor: change seconds to hours

--- src/provider/generate-refresh-token-provider.ts
+++ src/provider/generate-refresh-token-provider.ts
@@ -2,23 +2,23 @@ import { prisma } from "@/database/lib/prisma";
 import dayjs from "dayjs";
 
 export class GenerateRefreshTokenProvider {
-	public async execute(userId: string) {
-		if (!userId) throw new Error("Credenciais invalidas");
+  public async execute(userId: string) {
+    if (!userId) throw new Error("Credenciais invalidas");
 
-		const expiresIn = dayjs().add(15, "second").unix();
+    const expiresIn = dayjs().add(15, "hours").unix();
 
-		const generateToken = await prisma.refreshToken.create({
-			data: {
-				userId,
-				expiresIn,
-			},
-			select: {
-				id: true,
-				expiresIn: true,
-				userId: true,
-			},
-		});
+    const generateToken = await prisma.refreshToken.create({
+      data: {
+        userId,
+        expiresIn,
+      },
+      select: {
+        id: true,
+        expiresIn: true,
+        userId: true,
+      },
+    });
 
-		return generateToken;
-	}
+    return generateToken;
+  }
 }
