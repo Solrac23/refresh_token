75c9a28614298059ddb6b363c2831dccaf191736
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-05-31 18:07:11 -0300

    refactor: add select to permission and role

--- src/repositories/user-repository/i-user-repository.ts
+++ src/repositories/user-repository/i-user-repository.ts
@@ -1,5 +1,5 @@
 import type { User } from "@/database/generated/prisma/client";
 
 export interface IUserRepository {
-	findUserById(userId: string): Promise<User | null>;
+  findUserById(userId: string): Promise<Omit<User, "password"> | null>;
 }
--- src/repositories/user-repository/user-repository.ts
+++ src/repositories/user-repository/user-repository.ts
@@ -3,11 +3,48 @@ import { prisma } from "@/database/lib/prisma";
 import type { IUserRepository } from "./i-user-repository";
 
 export class UserRepository implements IUserRepository {
-	async findUserById(userId: string): Promise<User | null> {
-		return await prisma.user.findFirst({
-			where: {
-				id: userId,
-			},
-		});
-	}
+  public async findUserById(
+    userId: string,
+  ): Promise<Omit<User, "password"> | null> {
+    return await prisma.user.findFirst({
+      where: {
+        id: userId,
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        name: true,
+        createdAt: true,
+        updatedAt: true,
+        password: false,
+        usersPermissions: {
+          select: {
+            permission: {
+              select: {
+                id: true,
+                name: true,
+                description: true,
+                createdAt: true,
+                updatedAt: true,
+              },
+            },
+          },
+        },
+        usersRoles: {
+          select: {
+            role: {
+              select: {
+                id: true,
+                name: true,
+                description: true,
+                createdAt: true,
+                updatedAt: true,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
 }
