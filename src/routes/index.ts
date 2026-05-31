d5b6cc938950d0bcc15f6e4e05502be0a584ecd8
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-05-31 18:09:46 -0300

    feat: add route roles and permissions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,22 @@
 import type { FastifyTypedInstance } from "../fastify-typed-instance";
 import { authRoutes } from "./auth.routes";
 import { courseRoutes } from "./course.routes";
+import { permissionsRoutes } from "./permissions.routes";
+import { rolesRoutes } from "./roles.routes";
 import { userRoutes } from "./user.routes";
 
 export async function routes(app: FastifyTypedInstance) {
-  app.register(userRoutes);
+  app.register(userRoutes, {
+    prefix: "/users",
+  });
   app.register(authRoutes, {
     prefix: "/auth",
+  });
+  app.register(rolesRoutes, {
+    prefix: "/roles",
+  });
+  app.register(permissionsRoutes, {
+    prefix: "/permissions",
   });
   app.register(courseRoutes);
 }
