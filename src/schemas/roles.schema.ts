2808077ff95cd646ec1021cf0143177f35d4818e
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-05-31 18:03:15 -0300

    feat: add schema for ACL validation

--- src/schemas/roles.schema.ts
+++ src/schemas/roles.schema.ts
@@ -5,8 +5,31 @@ export const createRoleRequestSchema = z.object({
   description: z.optional(z.string()),
 });
 
+export const createRolePermissionRequestSchema = z.object({
+  permissions: z.array(z.string()),
+});
+
 export const createRoleResponseSchema = z.object({
   id: z.string(),
   name: z.string(),
   description: z.string(),
+});
+
+export const createRolePermissionResponseSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  createdAt: z.union([z.iso.datetime(), z.date()]),
+  updatedAt: z.union([z.iso.datetime(), z.date()]),
+  permissionsRoles: z.array(
+    z.object({
+      permission: z.object({
+        id: z.string(),
+        name: z.string(),
+        description: z.string(),
+        createdAt: z.union([z.iso.datetime(), z.date()]),
+        updatedAt: z.union([z.iso.datetime(), z.date()]),
+      }),
+    }),
+  ),
 });
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,10 +1,46 @@
 import * as z from "zod/mini";
 
 export const createUserRequestSchema = z.object({
-	name: z.string(),
-	username: z.string(),
-	email: z.email(),
-	password: z.string().check(z.minLength(6)),
+  name: z.string(),
+  username: z.string(),
+  email: z.email(),
+  password: z.string().check(z.minLength(6)),
+});
+
+export const createUserACLRequestSchema = z.object({
+  roles: z.array(z.string()),
+  permissions: z.array(z.string()),
+});
+
+export const createUserACLResponseSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  username: z.string(),
+  email: z.string(),
+  createdAt: z.union([z.iso.datetime(), z.date()]),
+  updatedAt: z.union([z.iso.datetime(), z.date()]),
+  usersPermissions: z.array(
+    z.object({
+      permission: z.object({
+        id: z.string(),
+        name: z.string(),
+        description: z.string(),
+        createdAt: z.union([z.iso.datetime(), z.date()]),
+        updatedAt: z.union([z.iso.datetime(), z.date()]),
+      }),
+    }),
+  ),
+  usersRoles: z.array(
+    z.object({
+      role: z.object({
+        id: z.string(),
+        name: z.string(),
+        description: z.string(),
+        createdAt: z.union([z.iso.datetime(), z.date()]),
+        updatedAt: z.union([z.iso.datetime(), z.date()]),
+      }),
+    }),
+  ),
 });
 
 export const createUserResponseSchema = z.object({
