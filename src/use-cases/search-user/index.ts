76a8bcd5dfa22af1ab64e4906cf283e94c62d616
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 20:45:39 -0300

    refactor: add Dependency Inversion User repository

--- src/use-cases/search-user/index.ts
+++ src/use-cases/search-user/index.ts
@@ -1,7 +1,8 @@
-import { SearchUserUseCase } from "./search-user-use-case";
+import { UserRepository } from "@/repositories/user-repository";
 import { SearchUserController } from "./search-user-controller";
+import { SearchUserUseCase } from "./search-user-use-case";
 
-const searchUserUseCase = new SearchUserUseCase();
+const searchUserUseCase = new SearchUserUseCase(new UserRepository());
 const searchUserController = new SearchUserController(searchUserUseCase);
 
-export { searchUserUseCase, searchUserController };
+export { searchUserController, searchUserUseCase };
