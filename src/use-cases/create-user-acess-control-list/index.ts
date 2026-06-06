3988c8e540ed63387dae3d57a9d67e74f0389f22
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 20:52:47 -0300

    refactor: add Dependency Inversion

--- src/use-cases/create-user-acess-control-list/index.ts
+++ src/use-cases/create-user-acess-control-list/index.ts
@@ -1,12 +1,20 @@
-import { CreateUserAcessControlListUseCase } from "./create-user-acess-control-list-use-case";
-import { CreateUserAcessControlListController } from "./create-user-acess-control-list-controller";
+import { PermissionRepository } from "@/repositories/permission-repository";
+import { RoleRepository } from "@/repositories/role-repository";
+import { UserPermissionsRepository } from "@/repositories/user-permissions-repository";
 import { UserRepository } from "@/repositories/user-repository";
+import { UserRolesRepository } from "@/repositories/user-roles-repository";
+import { CreateUserAcessControlListController } from "./create-user-acess-control-list-controller";
+import { CreateUserAcessControlListUseCase } from "./create-user-acess-control-list-use-case";
 
 const createUserACLUseCase = new CreateUserAcessControlListUseCase(
-  new UserRepository(),
+	new UserRepository(),
+	new PermissionRepository(),
+	new RoleRepository(),
+	new UserRolesRepository(),
+	new UserPermissionsRepository()
 );
 const createUserACLController = new CreateUserAcessControlListController(
-  createUserACLUseCase,
+	createUserACLUseCase
 );
 
 export { createUserACLController };
