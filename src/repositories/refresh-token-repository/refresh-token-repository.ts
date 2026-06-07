81abbf53423feb6a2c91f11245ae7cbdeeef69ef
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 22:01:24 -0300

    refactor: add queries create and delete

--- src/repositories/refresh-token-repository/refresh-token-repository.ts
+++ src/repositories/refresh-token-repository/refresh-token-repository.ts
@@ -3,21 +3,46 @@ import { prisma } from "@/database/lib/prisma";
 import type { IRefreshTokenRepository } from "./i-refresh-token-repository";
 
 export class RefreshTokenRepository implements IRefreshTokenRepository {
-	public async findRefreshTokenById(id: string): Promise<RefreshToken | null> {
-		return await prisma.refreshToken.findFirst({
-			where: {
-				id,
-			},
-		});
-	}
+  public async findRefreshTokenById(id: string): Promise<RefreshToken | null> {
+    return await prisma.refreshToken.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
 
-	public async deleteRefreshTokenById(
-		id: string
-	): Promise<RefreshToken | null> {
-		return await prisma.refreshToken.delete({
-			where: {
-				id,
-			},
-		});
-	}
+  public async deleteRefreshTokenById(
+    id: string,
+  ): Promise<RefreshToken | null> {
+    return await prisma.refreshToken.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  public async deleteByUserId(userId: string): Promise<void> {
+    await prisma.refreshToken.deleteMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  public async create(
+    userId: string,
+    expiresIn: number,
+  ): Promise<Pick<RefreshToken, "id" | "expiresIn" | "userId">> {
+    return await prisma.refreshToken.create({
+      data: {
+        userId,
+        expiresIn,
+      },
+      select: {
+        id: true,
+        expiresIn: true,
+        userId: true,
+      },
+    });
+  }
 }
