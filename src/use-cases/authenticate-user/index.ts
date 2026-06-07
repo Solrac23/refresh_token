ed634d2cced889a7c8ef05b7bdfbdb6fdd148b9b
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 21:18:09 -0300

    refactor: add dependency inversion

--- src/use-cases/authenticate-user/index.ts
+++ src/use-cases/authenticate-user/index.ts
@@ -1,5 +1,7 @@
 import { GenerateRefreshTokenProvider } from "@/provider/generate-refresh-token-provider";
 import { JwtProvider } from "@/provider/jwt/jwt-provider";
+import { RefreshTokenRepository } from "@/repositories/refresh-token-repository";
+import { UserRepository } from "@/repositories/user-repository";
 import type { FastifyInstance } from "fastify";
 import { Encryption } from "../util/encryption";
 import { AuthenticateUserController } from "./authenticate-user-controller";
@@ -9,7 +11,9 @@ export function authenticateUser(app: FastifyInstance) {
 	const authenticateUserUseCase = new AuthenticateUserUseCase(
 		new Encryption(),
 		new JwtProvider(app),
-		new GenerateRefreshTokenProvider()
+		new GenerateRefreshTokenProvider(),
+		new UserRepository(),
+		new RefreshTokenRepository()
 	);
 
 	const authenticateUserController = new AuthenticateUserController(
