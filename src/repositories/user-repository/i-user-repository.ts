a4f7dbdae87ac67e335ee5189ba00db95c42ebfe
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 21:58:18 -0300

    refactor: add create, find by id without relations and find by username

--- src/repositories/user-repository/i-user-repository.ts
+++ src/repositories/user-repository/i-user-repository.ts
@@ -2,4 +2,9 @@ import type { User } from "@/database/generated/prisma/client";
 
 export interface IUserRepository {
   findUserById(userId: string): Promise<Omit<User, "password"> | null>;
+  findByUsernameOrEmail(username: string, email: string): Promise<User | null>;
+  create(
+    data: Pick<User, "name" | "username" | "email" | "password">,
+  ): Promise<Pick<User, "id" | "name" | "username" | "email">>;
+  findByIdWithoutRelations(id: string): Promise<Omit<User, "password"> | null>;
 }
