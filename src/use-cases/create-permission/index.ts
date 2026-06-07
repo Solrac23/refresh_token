29f9c19ab23c90eeeb82d211842e8ce5931d6f1f
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 21:10:50 -0300

    refactor: add permission dependency inversion

--- src/use-cases/create-permission/index.ts
+++ src/use-cases/create-permission/index.ts
@@ -1,7 +1,10 @@
+import { PermissionRepository } from "@/repositories/permission-repository";
 import { CreatepermissionController } from "./create-permission-controller";
 import { CreatePermissionUseCase } from "./create-permission-use-case";
 
-const createPermissionUseCase = new CreatePermissionUseCase();
+const createPermissionUseCase = new CreatePermissionUseCase(
+  new PermissionRepository(),
+);
 const createPermissionController = new CreatepermissionController(
   createPermissionUseCase,
 );
