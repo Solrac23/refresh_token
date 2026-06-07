1fadc27bbf5c08d0e299769bb1959e94d8c06f25
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 22:02:51 -0300

    refactor: add create and delete by user id

--- src/repositories/refresh-token-repository/i-refresh-token-repository.ts
+++ src/repositories/refresh-token-repository/i-refresh-token-repository.ts
@@ -1,6 +1,11 @@
 import type { RefreshToken } from "@/database/generated/prisma/client";
 
 export interface IRefreshTokenRepository {
-	findRefreshTokenById(id: string): Promise<RefreshToken | null>;
-	deleteRefreshTokenById(id: string): Promise<RefreshToken | null>;
+  findRefreshTokenById(id: string): Promise<RefreshToken | null>;
+  deleteRefreshTokenById(id: string): Promise<RefreshToken | null>;
+  deleteByUserId(userId: string): Promise<void>;
+  create(
+    userId: string,
+    expiresIn: number,
+  ): Promise<Pick<RefreshToken, "id" | "expiresIn" | "userId">>;
 }
