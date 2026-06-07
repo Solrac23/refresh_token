8e3294dd622b01d8f3ac2666cb5069bc4c51daf0
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 21:05:14 -0300

    refactor: add dependency inversion

--- src/use-cases/create-role-permission/index.ts
+++ src/use-cases/create-role-permission/index.ts
@@ -1,7 +1,13 @@
 import { CreateRolePermissionController } from "./create-role-permission-controller";
 import { CreateRolePermissionUseCase } from "./create-role-permission-use-case";
 
-const createRolePermissionUseCase = new CreateRolePermissionUseCase();
+import { PermissionRepository } from "@/repositories/permission-repository";
+import { RoleRepository } from "@/repositories/role-repository";
+
+const createRolePermissionUseCase = new CreateRolePermissionUseCase(
+  new RoleRepository(),
+  new PermissionRepository(),
+);
 const createRolePermissionController = new CreateRolePermissionController(
   createRolePermissionUseCase,
 );
