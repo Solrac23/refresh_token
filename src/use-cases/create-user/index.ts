1704b7bfcf3646f90725a73866a717c2e5da9509
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 21:00:09 -0300

    refactor: add dependency inversion user repository

--- src/use-cases/create-user/index.ts
+++ src/use-cases/create-user/index.ts
@@ -1,8 +1,12 @@
+import { UserRepository } from "@/repositories/user-repository";
 import { Encryption } from "../util/encryption";
 import { CreateUserController } from "./create-user-controller";
 import { CreateUserUseCase } from "./create-user-use-case";
 
-const createUserUseCase = new CreateUserUseCase(new Encryption());
+const createUserUseCase = new CreateUserUseCase(
+	new Encryption(),
+	new UserRepository()
+);
 
 const createUserController = new CreateUserController(createUserUseCase);
 
