488402672459e0dcd9ee577d21b5c0c6390d740c
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 21:03:44 -0300

    refactor: add querys user repository

--- src/use-cases/create-user/create-user-use-case.ts
+++ src/use-cases/create-user/create-user-use-case.ts
@@ -1,43 +1,37 @@
-import { prisma } from "@/database/lib/prisma";
+import type { IUserRepository } from "@/repositories/user-repository/i-user-repository";
 import type { Encryption } from "../util/encryption";
 import type { IUserRequestDTO, IUserResponseDTO } from "./create-user-dto";
 
 export class CreateUserUseCase {
-	constructor(private encryption: Encryption) {}
+  constructor(
+    private encryption: Encryption,
+    private userRepository: IUserRepository,
+  ) {}
 
-	public async execute({
-		name,
-		username,
-		email,
-		password,
-	}: IUserRequestDTO): Promise<IUserResponseDTO | undefined> {
-		const userAlreadyExists = await prisma.user.findFirst({
-			where: {
-				OR: [{ username }, { email }],
-			},
-		});
+  public async execute({
+    name,
+    username,
+    email,
+    password,
+  }: IUserRequestDTO): Promise<IUserResponseDTO | undefined> {
+    const userAlreadyExists = await this.userRepository.findByUsernameOrEmail(
+      username,
+      email,
+    );
 
-		if (userAlreadyExists) {
-			throw new Error("User already exists");
-		}
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
 
-		const hashPassword = await this.encryption.hash(password);
+    const hashPassword = await this.encryption.hash(password);
 
-		const user = await prisma.user.create({
-			data: {
-				name,
-				username,
-				email,
-				password: hashPassword,
-			},
-			select: {
-				id: true,
-				name: true,
-				username: true,
-				email: true,
-			},
-		});
+    const user = await this.userRepository.create({
+      name,
+      username,
+      email,
+      password: hashPassword,
+    });
 
-		return { user };
-	}
+    return { user };
+  }
 }
