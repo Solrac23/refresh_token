0b3439c3b807b9b73ebf54b0fa505b6b64b1f489
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-06 21:07:08 -0300

    refactor: add dependency inversion

--- src/use-cases/create-role/index.ts
+++ src/use-cases/create-role/index.ts
@@ -1,7 +1,8 @@
+import { RoleRepository } from "@/repositories/role-repository";
 import { CreateRoleController } from "./create-role-controller";
 import { CreateRoleUseCase } from "./create-role-use-case";
 
-const createRoleUseCase = new CreateRoleUseCase();
+const createRoleUseCase = new CreateRoleUseCase(new RoleRepository());
 const createRoleController = new CreateRoleController(createRoleUseCase);
 
 export { createRoleController };
