41a7be095b45f85e23c1a182484beb75e31e5ad7
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-25 21:54:12 -0300

    refactor: issue #1 to organize encrypt password on compare

--- src/use-cases/util/encryption.ts
+++ src/use-cases/util/encryption.ts
@@ -1,23 +1,17 @@
 import { scryptSync } from "node:crypto";
 
 export class Encryption {
-	public async hash(password: string): Promise<string> {
-		const pass = scryptSync(
-			password,
-			process.env.SALT_PASSWORD as string,
-			Number(process.env.KEY_LENGTH)
-		);
+  public async hash(password: string): Promise<string> {
+    const pass = scryptSync(
+      password,
+      process.env.SALT_PASSWORD as string,
+      Number(process.env.KEY_LENGTH),
+    );
 
-		return pass.toString("hex");
-	}
+    return pass.toString("hex");
+  }
 
-	public async compare(password: string, hash: string): Promise<boolean> {
-		const pass = scryptSync(
-			password,
-			process.env.SALT_PASSWORD as string,
-			Number(process.env.KEY_LENGTH)
-		);
-
-		return pass.toString("hex") === hash;
-	}
+  public async compare(password: string, hash: string): Promise<boolean> {
+    return hash === (await this.hash(password));
+  }
 }
