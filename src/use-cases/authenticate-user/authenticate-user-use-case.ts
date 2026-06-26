5e277852aa4ba940abfaa723cc2f223f67cf7fd9
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-25 21:38:05 -0300

    chore: formatter code

--- src/use-cases/authenticate-user/authenticate-user-use-case.ts
+++ src/use-cases/authenticate-user/authenticate-user-use-case.ts
@@ -4,54 +4,54 @@ import type { IRefreshTokenRepository } from "@/repositories/refresh-token-repos
 import type { IUserRepository } from "@/repositories/user-repository/i-user-repository";
 import type { Encryption } from "../util/encryption";
 import type {
-	IAuthenticateUserRequestDTO,
-	IAuthenticateUserResponseDTO,
+  IAuthenticateUserRequestDTO,
+  IAuthenticateUserResponseDTO,
 } from "./authenticate-user-dto";
 
 export class AuthenticateUserUseCase {
-	constructor(
-		private encryption: Encryption,
-		private readonly jwtProvider: JwtProvider,
-		private readonly generateRefreshToken: GenerateRefreshTokenProvider,
-		private readonly userRepository: IUserRepository,
-		private readonly refreshTokenRepository: IRefreshTokenRepository
-	) {}
+  constructor(
+    private encryption: Encryption,
+    private readonly jwtProvider: JwtProvider,
+    private readonly generateRefreshToken: GenerateRefreshTokenProvider,
+    private readonly userRepository: IUserRepository,
+    private readonly refreshTokenRepository: IRefreshTokenRepository,
+  ) {}
 
-	public async execute({
-		username,
-		email,
-		password,
-	}: IAuthenticateUserRequestDTO): Promise<
-		IAuthenticateUserResponseDTO | unknown
-	> {
-		const user = await this.userRepository.findByUsernameOrEmail(
-			username,
-			email
-		);
+  public async execute({
+    username,
+    email,
+    password,
+  }: IAuthenticateUserRequestDTO): Promise<
+    IAuthenticateUserResponseDTO | unknown
+  > {
+    const user = await this.userRepository.findByUsernameOrEmail(
+      username,
+      email,
+    );
 
-		if (!user) {
-			throw new Error("User or password incorrect");
-		}
+    if (!user) {
+      throw new Error("User or password incorrect");
+    }
 
-		const isPasswordMatch = await this.encryption.compare(
-			password,
-			user.password
-		);
+    const isPasswordMatch = await this.encryption.compare(
+      password,
+      user.password,
+    );
 
-		if (!isPasswordMatch) {
-			throw new Error("User or password incorrect!");
-		}
+    if (!isPasswordMatch) {
+      throw new Error("User or password incorrect!");
+    }
 
-		const token = await this.jwtProvider.signToken({
-			id: user.id,
-			username: user.username,
-			email: user.email,
-		});
+    const token = await this.jwtProvider.signToken({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    });
 
-		await this.refreshTokenRepository.deleteByUserId(user.id);
+    await this.refreshTokenRepository.deleteByUserId(user.id);
 
-		const refreshToken = await this.generateRefreshToken.execute(user.id);
+    const refreshToken = await this.generateRefreshToken.execute(user.id);
 
-		return { token, refreshToken };
-	}
+    return { token, refreshToken };
+  }
 }
