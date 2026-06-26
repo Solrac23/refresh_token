55abbeda69fd0617aff022ddc7a20a877d1b8c55
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-25 21:45:18 -0300

    refactor: add middleware check permissions

--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -1,29 +1,28 @@
 import type { FastifyTypedInstance } from "@/fastify-typed-instance";
 import { authenticate } from "@/middlewares/authenticate";
-import { checkPermissions } from "@/middlewares/check-permissions";
 import { errorResponseSchema } from "@/schemas/error.schema";
 import { courseResponseSchema } from "@schemas/course.schema";
 
 export async function courseRoutes(app: FastifyTypedInstance) {
-  app.get(
-    "/courses",
-    {
-      onRequest: [authenticate, checkPermissions(["list_courses"])],
-      schema: {
-        tags: ["courses"],
-        description: "Get all courses",
-        response: {
-          200: courseResponseSchema,
-          401: errorResponseSchema,
-        },
-      },
-    },
-    async (request, reply) => {
-      return reply.send([
-        { id: 1, name: "Course 1" },
-        { id: 2, name: "Course 2" },
-        { id: 3, name: "Course 3" },
-      ]);
-    },
-  );
+	app.get(
+		"/courses",
+		{
+			onRequest: authenticate,
+			schema: {
+				tags: ["courses"],
+				description: "Get all courses",
+				response: {
+					200: courseResponseSchema,
+					401: errorResponseSchema,
+				},
+			},
+		},
+		async (request, reply) => {
+			return reply.send([
+				{ id: 1, name: "Course 1" },
+				{ id: 2, name: "Course 2" },
+				{ id: 3, name: "Course 3" },
+			]);
+		}
+	);
 }
