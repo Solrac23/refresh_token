af8bf74d8e22c73496b2761119f777c8d7a9a575
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-25 22:04:14 -0300

    refactor: rename folder from logger env dto to env type

--- src/logger/env-to-logger.ts
+++ src/logger/env-to-logger.ts
@@ -1,30 +1,30 @@
-import type { LoggerEnvType } from "./logger-env-dto";
+import type { LoggerEnvType } from "./logger-env-type";
 
 export const envToLogger: LoggerEnvType = {
-	development: {
-		transport: {
-			target: "pino-pretty",
-			options: {
-				translateTime: "SYS:HH:MM:ss Z",
-				ignore: "pid,hostname",
-			},
-		},
-		redact: ["req.headers.authorization"],
-		serializers: {
-			req(request) {
-				return {
-					host: request.ip,
-					method: request.method,
-					url: request.url,
-				};
-			},
-			res(reply) {
-				return {
-					statusCode: reply.statusCode,
-				};
-			},
-		},
-	},
-	production: true,
-	test: false,
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "SYS:HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+    redact: ["req.headers.authorization"],
+    serializers: {
+      req(request) {
+        return {
+          host: request.ip,
+          method: request.method,
+          url: request.url,
+        };
+      },
+      res(reply) {
+        return {
+          statusCode: reply.statusCode,
+        };
+      },
+    },
+  },
+  production: true,
+  test: false,
 };
