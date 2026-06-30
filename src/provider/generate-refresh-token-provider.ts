d1e609d8a58d25cdc9373841b661314a2f0ba3f7
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-29 21:07:43 -0300

    refactor: add repository interface refresh token

--- src/provider/generate-refresh-token-provider.ts
+++ src/provider/generate-refresh-token-provider.ts
@@ -1,14 +1,15 @@
-import { RefreshTokenRepository } from "@/repositories/refresh-token-repository";
+import type { IRefreshTokenRepository } from "@/repositories/refresh-token-repository";
 import dayjs from "dayjs";
 
 export class GenerateRefreshTokenProvider {
+  constructor(private refreshTokenRepository: IRefreshTokenRepository) {}
+
   public async execute(userId: string) {
     if (!userId) throw new Error("Credenciais invalidas");
 
     const expiresIn = dayjs().add(15, "hours").unix();
 
-    const refreshTokenRepository = new RefreshTokenRepository();
-    const generateToken = await refreshTokenRepository.create(
+    const generateToken = await this.refreshTokenRepository.create(
       userId,
       expiresIn,
     );
