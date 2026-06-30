94d9e9cd5f9426afa93eca4037d9e893087e403c
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-06-29 21:09:14 -0300

    refactor: change status code 201 to 200

--- src/use-cases/refresh-token-user/refresh-token-user-controller.ts
+++ src/use-cases/refresh-token-user/refresh-token-user-controller.ts
@@ -1,27 +1,27 @@
 import type {
-	RefreshTokenUserRequestDTO,
-	RefreshTokenUserResponseDTO,
+  RefreshTokenUserRequestDTO,
+  RefreshTokenUserResponseDTO,
 } from "@/use-cases/refresh-token-user/refresh-token-dto";
 import type { RefreshTokenUserUseCase } from "@/use-cases/refresh-token-user/refresh-token-user-use-case";
 import type { FastifyReply, FastifyRequest } from "fastify";
 
 export class RefreshTokenUserController {
-	constructor(private refreshTokenUserUseCase: RefreshTokenUserUseCase) {}
+  constructor(private refreshTokenUserUseCase: RefreshTokenUserUseCase) {}
 
-	public async handle(
-		request: FastifyRequest,
-		reply: FastifyReply
-	): Promise<RefreshTokenUserResponseDTO> {
-		const { refresh_token } = request.body as RefreshTokenUserRequestDTO;
+  public async handle(
+    request: FastifyRequest,
+    reply: FastifyReply,
+  ): Promise<RefreshTokenUserResponseDTO> {
+    const { refresh_token } = request.body as RefreshTokenUserRequestDTO;
 
-		try {
-			const token = await this.refreshTokenUserUseCase.execute(refresh_token);
-			return reply.code(201).send(token);
-		} catch (err: unknown) {
-			if (err instanceof Error) {
-				return reply.code(400).send({ error: err.message });
-			}
-			return reply.code(500).send({ error: err });
-		}
-	}
+    try {
+      const token = await this.refreshTokenUserUseCase.execute(refresh_token);
+      return reply.code(200).send(token);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        return reply.code(400).send({ error: err.message });
+      }
+      return reply.code(500).send({ error: err });
+    }
+  }
 }
