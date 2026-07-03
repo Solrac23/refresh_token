60d3f0dabad1cd7a4fccc2a8afb24d9d8e4ee7b4
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-07-03 00:57:03 -0300

    refactor: remove delete token by id and add query to token hash

--- src/repositories/refresh-token-repository/i-refresh-token-repository.ts
+++ src/repositories/refresh-token-repository/i-refresh-token-repository.ts
@@ -1,11 +1,7 @@
 import type { RefreshToken } from "@/database/generated/prisma/client";
 
 export interface IRefreshTokenRepository {
-  findRefreshTokenById(id: string): Promise<RefreshToken | null>;
-  deleteRefreshTokenById(id: string): Promise<RefreshToken | null>;
+  findRefreshTokenByHash(tokenHash: string): Promise<RefreshToken | null>;
   deleteByUserId(userId: string): Promise<void>;
-  create(
-    userId: string,
-    expiresIn: number,
-  ): Promise<Pick<RefreshToken, "id" | "expiresIn" | "userId">>;
+  create(userId: string, tokenHash: string, expiresIn: number): Promise<void>;
 }
