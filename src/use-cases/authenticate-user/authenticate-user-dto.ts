4c1cce8e6a5c8a4ecf60678df49fc571b2f20d4f
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-07-03 01:09:24 -0300

    refactor: change token to acessToken and refreshToken object to only string

--- src/use-cases/authenticate-user/authenticate-user-dto.ts
+++ src/use-cases/authenticate-user/authenticate-user-dto.ts
@@ -1,14 +1,10 @@
 export interface IAuthenticateUserRequestDTO {
-	username: string;
-	email: string;
-	password: string;
+  username: string;
+  email: string;
+  password: string;
 }
 
 export interface IAuthenticateUserResponseDTO {
-	token: string;
-	refreshToken: {
-		id: string;
-		expiresIn: number;
-		userId: string;
-	};
+  acessToken: string;
+  refreshToken: string;
 }
